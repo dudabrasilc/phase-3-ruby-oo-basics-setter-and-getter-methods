efa139fd854be9ad39f968fcab8f233cfd9cdf73
Maria Coitinho <brasildu@gmail.com>
2022-11-01 04:19:56 -0500

    commit tree df01a7d44b9d4e5c9dbf52f42e1bf4925325062e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Dog
 
+# getter method
+  def name
+    @name
+  end
+
+# setter method
+  def name=(name)
+    @name = name
+  end
+
+# setter method
+  def breed=(breed)
+    @breed = breed
+  end
+
+# getter method
+  def breed
+    @breed
+  end
+
 end
+
+mimi = Dog.new
+mimi.name = "Mimi"
+mimi.breed = "Pomeranian"
+
+# binding.pry
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 class Person
 
+  # setter method
+  def name=(name_value)
+    @name = name_value
+  end
+
+  # getter method
+  def name
+    @name
+  end
+
+  # setter method
+  def job=(job_value)
+    @job = job_value
+  end
+
+  # getter method
+  def job
+    @job
+  end
+
 end
+
+# binding.pry
